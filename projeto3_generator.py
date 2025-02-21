20b481f39d08aa919b5b9a5d7dbc02a8d5e92304
Yamandú Germano Cavalcanti <yamandu.germano@gmail.com>
2025-02-21 17:44:32 -0300

    inicio gerador de pacotes

--- projeto3_generator.py
+++ projeto3_generator.py
@@ -1,4 +1,15 @@
-def datagram_generator():
-    # 12 Bytes
-    # H1 , H2 , H3 , H4 , H5 , H6 , H7 , H8 , H9
+import struct
+
+def datagram_generator(
+    tipo:int,
+    configs:dict,
+    payload:bytearray) -> bytearray:
+    """
+    **Tipo 1:** [n_servidor,n_pacotes]
+    
+    **Tipo 2:** []
+    
+    **Tipo 3:** [id_pacote,n_pacotes,tamanho_pl]
+    """
+    
     pass
