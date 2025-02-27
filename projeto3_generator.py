67cc0cec053e105c43a0599da5da652d728d92c9
Yamandú Germano Cavalcanti <yamandu.germano@gmail.com>
2025-02-27 15:08:56 -0300

    gerador head tipo 1

--- projeto3_generator.py
+++ projeto3_generator.py
@@ -3,37 +3,23 @@ from struct import pack,unpack
 class DatagramGenerator:
     """
     ## Tipos de mensagens:
-    **Tipo 1:** [n_servidor,n_pacotes]
+    **Tipo 1:** [tipo<1> , n_servidor<1> , n_pacotes<4>]
     - Mensagem de solicitação de transmissão
     
-    **Tipo 2:** []
+    **Tipo 2:** [tipo<1>]
     - Mensagem de confirmação de transmissão
     
-    **Tipo 3:** [id_pacote,n_pacotes,tamanho_pl]
+    **Tipo 3:** [tipo<1> , id_pacote<4> , n_pacotes<4> , tamanho_pl<1>]
     - Pacote de dados
     
-    **Tipo 4:** [id_pacote]
+    **Tipo 4:** [tipo<1> , id_pacote<4>]
     - Mensagem de confirmação de pacote
     
-    **Tipo 5:** []
+    **Tipo 5:** [tipo<1>]
     - Mensagem de timeout
     
-    **Tipo 6:** [id_pacote]
+    **Tipo 6:** [tipo<1> , id_pacote<4>]
     - Mensagem de erro de pacote
-    
-    ---
-    
-    ## Payload Format
-    h0 - tipo de mensagem
-    h1 - livre
-    h2 - livre
-    h3 - número total de pacotes do arquivo
-    h4 - número do pacote sendo enviado
-    h5 - se tipo for handshake: id do arquivo (crie um)
-    h5 - se tipo for dados: tamanho do payload
-    h6 - pacote solicitado para recomeço quando a erro no envio.
-    h7 - último pacote recebido com sucesso.
-    h8 - h9 - CRC (Por ora deixe em branco. Fará parte do projeto 5)
     """
     def __init__(self):
         self.tipos = {
@@ -42,14 +28,14 @@ class DatagramGenerator:
 
     def generate_header(self,
         tipo:int,
-        configs:dict,
-        payload:bytearray) -> bytearray:
+        configs:dict) -> bytearray:
         header = bytearray(10)
-        header[0] = 1
+        header[0] = tipo
         
-    def generate_payload(self,
-        data) -> bytearray:
-        pass
+        if tipo in self.tipos.keys():
+            header = self.tipos[tipo](header,configs)
+        
+        return header
     
     def type1(header:bytearray, configs):
         bytestring = b''
@@ -57,6 +43,11 @@ class DatagramGenerator:
             header[0].to_bytes()+                # 1byte
             pack('!B',configs['id_servidor'])+   # 1byte
             pack('!I',configs['n_pacotes'])+     # 4bytes
-            bytearray(4))                        # 4bytes
+            bytearray(6))                        # 6bytes
         
         return bytestring
+    
+    
+    def generate_payload(self,
+        data) -> bytearray:
+        pass
